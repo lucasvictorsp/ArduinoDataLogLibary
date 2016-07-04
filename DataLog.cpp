bf27f0a673447d7dceb4560915f4c273d9a62e99
lucasvsilvap <lucasvsilvap@gmail.com>
2016-07-04 18:47:27 -0300

    LM35 e DHT implementados.

--- DataLog.cpp
+++ DataLog.cpp
@@ -1,32 +1,99 @@
 #include "Datalog.h"
 
-DataLog::DataLog(){
+void Datalog::init() {
+	qtdDHT = 0;
+	qtdLM35 = 0;
+	qtdDS18s20 = 0;
+}
 
+DataLog::DataLog(){
+	fileName = "medidas.txt";
+	init()
 }
 
 DataLog::DataLog( String s) {
 	fileName = s;
-	qtdDHT = 0;
-
+	init()
 }
 
 int DataLog::addDHT(int tipo, int pino, String nome) {
 	DHT dht(pino, tipo);
 	if ( qtdDHT < MAX_SENSORES) {
-		dhtObj[qtdDHT].obj = dht;
-		dhtObj[qtdDHT].id = nome;
+		DHTs[qtdDHT].obj = dht;
+		DHTs[qtdDHT].id = nome;
 		dht.begin();
 		qtdDHT++;
+	} else {
+		Serial.println("Numero maximo de DHTs ultrapassado! Ignorando...");
 	}
+	return qtdDHT;
 }
-int DataLog::addDS18d20(int , String){
+
+int DataLog::addDS18d20(int , String) {
 
 }
-int DataLog::addLM35(int , String){
 
+int DataLog::addLM35(int , String) {
+	lm35 lm(pino);
+	if ( qtdLM35 < MAX_SENSORES) {
+		LM35s[qtdLM35].obj = lm;
+		LM35s[qtdLM35].id = nome;
+		qtdLM35++;
+	} else {
+		Serial.println("Numero maximo de LM35 ultrapassado! Ignorando...");
+	}
+	return qtdLM35;
 }
+
 void DataLog::logFile( String s ){
 
+}
+
+void DataLog::header() {
+	String linha;
+	for (int i=0; i < qtdDHT; i++) {
+		linha += "DHT_" + dhtObj[i].id + "; ";
+	}
+
+	for (int i=0; i < qtdLM35; i++) {
+		linha += "LM35_" + Lm35Obj[i].id + "; ";
+	}
+
+	writeToFile(linha);
+}
+
+bool DataLog::commit() {
+	String linha;
+	for (int i=0; i < qtdDHT; i++) {
+		linha += dhtObj[i].obj.readTemperature() + "; ";
+	}
+
+	for (int i=0; i < qtdLM35; i++) {
+		linha += Lm35Obj[i].obj.MeasureTemp() + "; ";
+	}
+	
+	return writeToFile(linha);
+}
+
+bool DataLog::writeToFile(String linha) {
+	File arquivo;
+	if (!SD.begin(4)) {
+		Serial.println("Falha ao inicializar modulo SD!");
+		return false;
+	}
+	Serial.println("Modulo SD inicializado.");
+
+	arquivo = SD.open(fileName, FILE_WRITE);
+	if (arquivo) {
+	    Serial.println("Escrevendo no arquivo...");
+	    arquivo.println(linha);
+	    arquivo.close();
+	} else {
+	    Serial.println("Erro abrindo o arquivo de DataLog.");
+	    return false;
+	}
+
+	return true;
 }
 
 DataLog::~DataLog(){
--- DataLog.h
+++ DataLog.h
@@ -6,25 +6,35 @@
 #include <DHT.h> // "DHT.h"
 #include <OneWire.h>
 #include <DallasTemperature.h>
-//#include <String.h>
+#include <SPI.h>
+#include <SD.h>
 
 #define MAX_SENSORES 10
 
 class DataLog {
 	struct DhtObj{ DHT obj; String id};
+	struct Lm35Obj{ lm35 obj; String id};
+	struct DS18s20_Obj{ DS18s20 obj; String id};
 	public:
+		void init();
 		DataLog();
 		DataLog(String );
 		int addDHT(int , int , String );
 		int addDS18d20(int , String );
 		int addLM35(int , String );
-		void logFile(String ); // Arequivo de saIDA 
+		void logFile(String ); // Arquivo de saida
+		bool commit();
+		bool writeToFile(String );
 		~DataLog();
 
 	private:
 		String fileName;
-		dhtObj dhts[10];
+		dhtObj DHTs[10];
+		Lm35Obj LM35s[10];
+		DS18s20_Obj DS18s[10];
 		int qtdDHT;
+		int qtdLM35;
+		int qtdDS18s20;
 };
 
 #endif	/* DATALOG_H */
